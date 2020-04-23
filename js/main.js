352af7f148cfabc809a27b6a040489bd8e3c67e8
aspienadela <54401524+aspienadela@users.noreply.github.com>
2020-04-22 23:39:14 -0700

    htaccess and preloader
    update the htaccess and add the new preloader feature of the website

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+$(document).ready(function() {
+    setTimeout(function() {
+        $('.wrapper').addClass('loaded');      
+    }, 3000);
+});
+
+jQuery(function(){
+    $(window).load(function(){    
+        $('.wrapper').removeClass('preload');    
+    });  
+});  
+
 //demo 01
 $("#about").animatedModal({
     color:'#000'
